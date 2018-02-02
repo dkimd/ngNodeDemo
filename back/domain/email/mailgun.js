848858b3a12e02a8c3e0c0b01b5d5bdcb20f570c
david.kim <taewdy@gmail.com>
2018-02-02 22:55:42 +1100

    mailgun config function change

--- back/domain/email/mailgun.js
+++ back/domain/email/mailgun.js
@@ -1,18 +1,35 @@
 const querystring = require('querystring');
 
+var mailgun = function (email) {
+  console.log(email);
+  var content = setMailContent(email);
+
+  var mail = {
+    body: content,
+    header: setMailHeader(content)
+  };
+  
+  return mail;
+}
+
 function setMailContent(email) {
-  const mailBody = {
-    from: "Mailgun Sandbox <postmaster@sandbox0c5f5f70caac44ddad36143ff3bce35c.mailgun.org>", 
+  console.log("["+email.recipients+"]");
+  console.log("["+email.cc+"]");
+  var mailBody = {
+    from: 'postmaster@sandbox0c5f5f70caac44ddad36143ff3bce35c.mailgun.org',     
     to: email.recipients,
+    cc: email.recipients,
+    //bcc: email.bcc,
     subject: email.subject,
     text: email.contents
   };
-  return querystring.stringify(mailBody);
+  console.log(mailBody);
+  return querystring.stringify(mailBody);  
 }
 
-function setMailHeader() {
+function setMailHeader(content) {
   const base64encodedData = new Buffer(process.env.MAILGUN_ID_PWD).toString('base64');  
-
+  console.log("content[[[[[[[" + content + "]]]]]");
   const options = {
     hostname: 'api.mailgun.net',
     port: 443,    
@@ -20,22 +37,25 @@ function setMailHeader() {
     method: 'POST',
     headers: {
       'Authorization': 'Basic ' + base64encodedData,
-      'Content-Type': 'application/x-www-form-urlencoded'    
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'content-length': Buffer.byteLength(content)    
     }    
   };
   console.log(options);
   return options;
 }
 
-var setMail = function (email) {  
-  return setMailContent(email)
-}
+// var setMail = function (email) {  
+//   return setMailContent(email)
+// }
 
-var setHeader = function () {  
-  return setMailHeader()
-}
+// var setHeader = function () {  
+//   return setMailHeader()
+// }
 
-module.exports = {
-  setMail: setMail,
-  setHeader: setHeader
-};
+module.exports = mailgun;
+
+// module.exports = {
+//   setMail: setMail,
+//   setHeader: setHeader
+// };
